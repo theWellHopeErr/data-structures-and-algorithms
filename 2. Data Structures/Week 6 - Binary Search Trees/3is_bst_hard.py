89316b6f7644025cfdf40e191d6aa384fb8fc030
theWellHopeErr <ssuryarajan@gmail.com>
2020-05-12 13:42:30 +0530

    Add Week 6

--- 2. Data Structures/Week 6 - Binary Search Trees/3is_bst_hard.py
+++ 2. Data Structures/Week 6 - Binary Search Trees/3is_bst_hard.py
@@ -5,7 +5,7 @@ threading.stack_size(2**27)  # new thread will get stack of such size
 def IsBinarySearchTree(j, mn, mx):
   if not j in tree: return True
   if tree[j][0] < mn or tree[j][0] > mx: return False
-  return IsBinarySearchTree(tree[j][1], mn, tree[j][0] - 1) and IsBinarySearchTree(tree[j][2], tree[j][0] + 1, mx)
+  return IsBinarySearchTree(tree[j][1], mn, tree[j][0] - 1)  and IsBinarySearchTree(tree[j][2], tree[j][0], mx)
 def main():
   nodes = int(sys.stdin.readline().strip())
   global tree
